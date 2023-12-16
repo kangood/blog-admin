fb0ca53a27c7227beca05adbbd9cafd3afc01876
kangod <kangod.yan@gmail.com>
2023-12-16 10:46:24 +0800

    modify: 1、修改访问路径

--- src/http/axios/service.ts
+++ src/http/axios/service.ts
@@ -3,9 +3,15 @@ import { isNil } from 'lodash';
 
 import { globalError } from '@/utils/arcod-extract';
 
+// 保存环境变量
+const isPrd = process.env.NODE_ENV === 'production';
+
+// 区分开发环境还是生产环境基础URL
+export const basicUrl = isPrd ? 'https://admin.kangod.top' : 'http://127.0.0.1:6601';
+
 // 设置axios的额外配置
 export const service = axios.create({
-    baseURL: `http://127.0.0.1:6601/api`
+    baseURL: `${basicUrl}/api`
 });
 // 拦截请求处理
 service.interceptors.request.use(async (params) => {
